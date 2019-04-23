8f5d002c7a50dccd0b6f56b9616ac846ce209a76
Abhijeet Kinjawadekar <abhijeetgk@gmail.com>
2019-04-23 18:15:59 +0530

    templates and error handling

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
+const createError = require('http-errors');
 
 const app = express();
+
 app.set('view engine', 'pug');
 if (app.get('env') == "development"){
     app.locals.pretty=true;
@@ -14,6 +16,19 @@ app.get('/favicon.ico', (req, res, next) => {
 });
 app.use('/', routes());
 
+app.use((req,res,next)=>{
+    return next(createError(404,'File not found'));
+});
+
+app.use((err,req,res,next)=>{
+    res.locals.message = err.message;
+    const status = err.status || 500;
+    res.locals.status = status;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(status);
+    return res.render('error');
+
+})
 app.listen(3000);
 
 module.export = app;
